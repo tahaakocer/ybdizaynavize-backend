8ddf5e0eae8a2ea47de190ebd5f3c087e08c0368
tahaakocer <taha.kocer317@gmail.com>
2024-10-10 16:55:14 +0300

    brands completed

--- src/main/java/com/tahaakocer/ybdizaynavize/model/product/Brand.java
+++ src/main/java/com/tahaakocer/ybdizaynavize/model/product/Brand.java
@@ -1,10 +1,7 @@
 package com.tahaakocer.ybdizaynavize.model.product;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -12,6 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "brands")
+@Builder
 public class Brand {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
